523da50a1765d09122c48fcafa7f6820360b8691
Fists <xfistsisunbeatable@gmail.com>
2025-10-31 19:06:31 +0300

    Initial commit

---
+++ src/main/java/com/lf/create_extras/CreateExtrasDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lf.create_extras;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateExtrasDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
