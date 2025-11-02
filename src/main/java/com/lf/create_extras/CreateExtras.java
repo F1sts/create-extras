c6ecd96a3698dd5c387fe8087081db5a606cb024
Fists <xfistsisunbeatable@gmail.com>
2025-11-02 17:26:17 +0300

    Added initializer for BlockEntityManager, BlockManager, Registrate

--- src/main/java/com/lf/create_extras/CreateExtras.java
+++ src/main/java/com/lf/create_extras/CreateExtras.java
@@ -1,16 +1,35 @@
 package com.lf.create_extras;
 
+import com.lf.create_extras.helpers.ShaftPlacementHelper;
+import com.lf.create_extras.managers.BlockEntityManager;
+import com.lf.create_extras.managers.BlockManager;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import dev.engine_room.flywheel.lib.model.baked.PartialModel;
+import net.createmod.catnip.placement.PlacementHelpers;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CreateExtras implements ModInitializer {
     public static final String MOD_ID = "create_extras";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MOD_ID);
+
+    public static final int SHAFT_PLACEMENT_HELPER = PlacementHelpers.register(new ShaftPlacementHelper());
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello Fabric world!");
+        BlockEntityManager.registerBlockEntities();
+        BlockManager.registerBlocks();
+        REGISTRATE.register();
+    }
+
+    public static ResourceLocation asResource(String path) {
+        return new ResourceLocation(MOD_ID, path);
+    }
+    public static PartialModel getPartialModel(String path) {
+        return PartialModel.of(asResource(path));
     }
 }
